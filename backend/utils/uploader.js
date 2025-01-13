a5f9f8d8aab623e27cb4e8540a54019d06e81d0a
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-13 21:39:44 +0530

    multer configured

--- backend/utils/uploader.js
+++ backend/utils/uploader.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Defining storage for the uploaded files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public//uploads");
+    cb(null, "./uploads");
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
   },
 });
 
-// Creating the multer instance
-const upload = multer({ storage: storage });
+// Creating the multer instance to accept multiple files (pictures)
+const upload = multer({ storage: storage }).array("pictures", 10);
 
 module.exports = upload;
