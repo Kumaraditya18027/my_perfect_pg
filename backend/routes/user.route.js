d94f77bccc721798599b66f8447ef4b62bdc73f5
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-19 18:33:09 +0530

    routes added

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -7,13 +7,17 @@ const {
 } = require("../controllers/user/auth.controller");
 const { getAllPgs } = require("../controllers/customer/pg.controller.js");
 const authHandler = require("../middlewares/authHandler.js");
+const { createBooking } = require("../controllers/admin/pg.controller.js");
 
 //authentication
 router.route("/user-register").post(registerUser);
 router.route("/user-login").post(loginUser);
-router.route("/user-logout").post(authHandler, logoutUser);
+router.route("/user-logout").post(authHandler(), logoutUser);
 
 //get all pgs
 router.route("/user/get-all-pgs").post(getAllPgs);
+
+//pg bookings
+router.route("/user/create-booking").post(authHandler(), createBooking);
 
 module.exports = router;
