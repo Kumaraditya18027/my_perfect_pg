edb4351d9f8ca62a3c990b5c8400186414b4b9f9
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-20 14:02:13 +0530

    Auth context updated

---
+++ frontend2/src/app/contexts/AuthContext.tsx
@@ -0,0 +1,49 @@
+// context/AuthContext.tsx
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+import { useRouter } from "next/navigation";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  login: (token: string, userType: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const login = (token: string, userType: string) => {
+    if (token && userType) {
+      localStorage.setItem("authToken", token);
+      localStorage.setItem("isLoggedIn", true);
+      setIsAuthenticated(true);
+      router.push("/Pg"); // Replace with your desired route
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    setIsAuthenticated(false);
+    router.push("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
