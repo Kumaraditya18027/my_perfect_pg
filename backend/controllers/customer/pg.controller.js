1fecc0215f814f68872c6774496789dccbd1f572
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-23 19:26:21 +0530

    fetch pg details

--- backend/controllers/customer/pg.controller.js
+++ backend/controllers/customer/pg.controller.js
@@ -1,12 +1,15 @@
 const Pg = require("../../models/pg.model");
 const asyncHandler = require("../../utils/asyncHandler");
+const { NotFoundError } = require("../../utils/customErrorHandler");
 const ResponseHandler = require("../../utils/responseHandler");
 
 //get all pgs
 const getAllPgs = asyncHandler(async (req, res) => {
   const pgs = await Pg.find({
     $and: [{ isAdminVerified: true }, { deleted: false }],
-  }).select("-_id");
+  }).select(
+    "-_id uuid name address gender rooms services description rating timings pictures location.latitude location.longitude"
+  );
 
   if (!pgs || pgs.length === 0) {
     return res
@@ -21,10 +24,33 @@ const getAllPgs = asyncHandler(async (req, res) => {
     );
 });
 
+//get specific pg details
+const getPgDetails = asyncHandler(async (req, res) => {
+  const { pgId } = req.params;
+
+  // Find the PG by UUID
+  const pg = await Pg.findOne({
+    uuid: pgId,
+  }).select("-_id");
+
+  // Check if the PG exists
+  if (!pg) {
+    return res
+      .status(404)
+      .json(new ResponseHandler(404, "PG not found or not available!"));
+  }
+
+  // Return success response
+  return res
+    .status(200)
+    .json(new ResponseHandler(200, "PG details fetched successfully!", pg));
+});
+
 //book a pg visit
 const bookPgVisit = asyncHandler(async (req, res) => {});
 
 module.exports = {
   getAllPgs,
   bookPgVisit,
+  getPgDetails,
 };
