ae22832b601a18b83b37e845ec4eb26d450ba5cb
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-23 19:26:40 +0530

    fixed issues and added new route

--- backend/routes/customer.route.js
+++ backend/routes/customer.route.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authHandler = require("../middlewares/authHandler.js");
 const { logoutUser } = require("../controllers/user/auth.controller");
-const { getAllPgs } = require("../controllers/customer/pg.controller.js");
+const {
+  getAllPgs,
+  getPgDetails,
+} = require("../controllers/customer/pg.controller.js");
 
 //authentication
-router.route("/customer-logout").post(authHandler, logoutUser);
+router.route("/customer-logout").post(authHandler(), logoutUser);
 
 //pg
-router.route("/customer/get-all-pgs").post(authHandler, getAllPgs);
+router.route("/customer/get-all-pgs").get(authHandler(), getAllPgs);
+router.route("/customer/get-pg-details/:pgId").get(authHandler(), getPgDetails);
 
 module.exports = router;
