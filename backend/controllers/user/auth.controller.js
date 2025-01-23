655075aac50f7595735ab402202ef05206101d88
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-23 23:32:48 +0530

    auth controller updated

--- backend/controllers/user/auth.controller.js
+++ backend/controllers/user/auth.controller.js
@@ -115,12 +115,13 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: false,
+    sameSite: "Strict", // Prevents CSRF attacks
     expires: new Date(Date.now() + 60 * 60 * 24 * 1000),
+    path: "/",
   };
 
   return res
     .status(200)
-    .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(
       new ResponseHandler(201, "User logged in successfully", {
