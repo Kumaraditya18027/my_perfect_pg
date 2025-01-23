4d2625d9f636e20635655f2163d50e90d521f8e6
Anirban Dutta <meanirbandutta22@gmail.com>
2025-01-23 15:54:02 +0530

    pgowner dashboard page updated

--- frontend2/src/app/pgowner/page.tsx
+++ frontend2/src/app/pgowner/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { useAuth } from "../contexts/AuthContext";
+
 // app/pgowner/page.tsx
 const PgOwnerHome = () => {
+  const { currentUserData } = useAuth();
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome, PG Owner!</h1>
+      <h1 className="text-2xl font-bold">
+        Welcome, {currentUserData?.name || "PG Owner"}!
+      </h1>
       <p>Select an option from the navbar to manage your PG.</p>
     </div>
   );
