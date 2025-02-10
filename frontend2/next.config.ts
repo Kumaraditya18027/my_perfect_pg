350ededaac320468e5098a255bc8744f8b54fa3c
Anirban Dutta <meanirbandutta22@gmail.com>
2025-02-10 13:44:33 +0530

    image src added

--- frontend2/next.config.ts
+++ frontend2/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "th.bing.com",
+      },
+    ],
     domains: ["res.cloudinary.com"], // Add Cloudinary domain here
   },
   eslint: {
